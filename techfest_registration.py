de9f629dea3fd52ee2852c84445c369310caa197
[Lei] <[leiivan.villacorta@benilde.edu.ph]>
2025-11-06 12:06:46 +0800

    Added nested dictionary

--- techfest_registration.py
+++ techfest_registration.py
@@ -1,6 +1,16 @@
 dict ={}
 
 print("Welcome to SMIT TechFest!")
-print("Event organized by Lei Villacorta of APPDAET BTCS2")
+print("Event organized by Lei Villacorta of APPDAET BTCS2\n")
 
-noPartic = input("How many participants will register? ")
+noPartic = int(input("How many participants will register? "))
+if noPartic <= 0: exit()
+
+for i in range(noPartic):
+    pName = input("\nEnter participant name: ")
+    pTrack = input("Enter chosen track: ")
+
+    dict[f"Partcipant{i+1}"] = {
+        "Name": pName,
+        "Track": pTrack,
+    }
