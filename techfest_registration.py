91f133439b1579d63255596c6c7adf75b870c4a6
[Lei] <[leiivan.villacorta@benilde.edu.ph]>
2025-11-06 12:56:17 +0800

    Finished Task 4 - added duplicate names

--- techfest_registration.py
+++ techfest_registration.py
@@ -1,4 +1,6 @@
+allNames = []
 dict = {}
+names = set({})
 tracks = set({})
 
 print("Welcome to SMIT TechFest!")
@@ -10,19 +12,38 @@ if noPartic <= 0: exit()
 for i in range(noPartic):
     pName = input("\nEnter participant name: ")
     pTrack = input("Enter chosen track: ")
+    allNames.append(pName)
 
-    dict[f"Partcipant{i+1}"] = {
+    dict[f"Partcipant{i + 1}"] = {
         "Name": pName,
         "Track": pTrack,
     }
 
-print(f"\nRegistered participants")
+print(f"\nRegistered participants:")
 for i in range(len(dict)):
-    tracks.add(dict[f'Partcipant{i+1}']['Track'])
-    print(f"{i+1}. {dict[f'Partcipant{i+1}']['Name']} - {dict[f'Partcipant{i+1}']['Track']}")
+    names.add(dict[f'Partcipant{i + 1}']["Name"])
+    tracks.add(dict[f'Partcipant{i + 1}']['Track'])
+    print(
+        f"{i + 1}. {dict[f'Partcipant{i + 1}']['Name']} - {dict[f'Partcipant{i + 1}']['Track']}")
 
-print("\nTracks offered in this event: ")
+print("\nTracks offered in this event:")
 if len(tracks) < 2:
     print("Not enough variety in tracks.")
 else:
     print(", ".join(str(i) for i in tracks))
+
+if len(names) < len(allNames):
+    print("\nDuplicate names found:")
+    check = set()
+    duplicates = set()
+    for i in allNames:
+        if i in check:
+            duplicates.add(i)
+        check.add(i)
+
+    for i in duplicates:
+        print(i)
+else:
+    print("No duplicate names.")
+
+print("\nParticipants per Track:")
